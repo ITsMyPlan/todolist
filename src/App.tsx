9523a9c9e93f1efef83a42eb8123fd1d626d7dba
choidabom <dabomi0305@naver.com>
2024-06-01 00:56:39 +0900

    fix. tailwind 적용 오류 (이전에 생성된 tailwind.config.js와 postcoss.config.js로 인해 npx tailwindcss init -p가 제대로 적용 x)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 const App = (): JSX.Element => {
     return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-    // return <div className="bg-red-400">init project setting</div>;
 };
 
 export default App;
