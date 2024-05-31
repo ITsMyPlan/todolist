be99b474232f58fbad4afc5920cf76b48ff07f75
choidabom <dabomi0305@naver.com>
2024-06-01 00:04:17 +0900

    add. postcss, autoprefixer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 const App = (): JSX.Element => {
-    return <div className="bg-red-400">init project setting</div>;
+    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    // return <div className="bg-red-400">init project setting</div>;
 };
 
 export default App;
