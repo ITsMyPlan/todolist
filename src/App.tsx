c8cfbc7c6070e115060e027d00be9d49af4c20b8
choidabom <dabomi0305@naver.com>
2024-06-03 17:08:08 +0900

    add: storage(InMemory, Local, Session Storage)클래스 생성 및 다크 모드 적용 / tailwind의 dark 모드 세팅 기능 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
-import { desktopBackground } from './config/background';
+import { useEffect, useState } from 'react';
+import { safeLocalStorage } from './utils/storage';
 
 const App = (): JSX.Element => {
-    const dark = false; // TODO:: 다크모드
+    const localStorageChecker = (): boolean => {
+        const theme = safeLocalStorage.getItem('theme');
+        return theme === 'dark';
+    };
+
+    const [dark, setDark] = useState(localStorageChecker());
+
+    const darkSetButton = (): void => {
+        setDark((state) => {
+            const update = !state;
+            if (update) {
+                safeLocalStorage.setItem('theme', 'dark');
+            } else {
+                safeLocalStorage.setItem('theme', 'light');
+            }
+            return update;
+        });
+    };
+
+    useEffect(() => {
+        const theme = safeLocalStorage.getItem('theme');
+        const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        if (theme === 'dark' || (!theme && prefersDark)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    }, [dark]);
 
     return (
-        <div
-            className="bg-cover h-screen w-screen"
-            style={{
-                backgroundImage: `url(${dark ? desktopBackground.dark : desktopBackground.light})`,
-            }}
-        ></div>
+        <div className="bg-light dark:bg-dark bg-cover h-screen w-screen">
+            <button onClick={darkSetButton}>{dark ? 'Dark Mode' : 'Light Mode'}</button>
+        </div>
     );
 };
 
