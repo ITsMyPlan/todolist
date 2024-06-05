ec26d6d52e0003c66fd31b3582b18771541f0f30
choidabom <48302257+choidabom@users.noreply.github.com>
2024-06-05 22:17:59 +0900

    add: tailwindcss fontFamily web font 적용 (#4)
    * add: tailwind.config.js -fontFamily custom font 추가 (Apple SD Gothic Neo, Pretendard)
    
    * add: font download 속도를 측정하기 위한 test web font 추가
    
    * delete: deprecated desktopBackground object
    
    * add: font-display: swap (응답이올때까지무한정기다리고그전까진기본폰트를보여줌)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,16 @@ export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     darkMode: 'class',
     theme: {
+        fontFamily: {
+            apple: ['Apple SD Gothic Neo'],
+            appleBold: ['Apple SD Gothic Neo Bold'],
+            pretendard: ['Pretendard-Regular'],
+        },
         extend: {
             backgroundImage: {
                 light: `url(./assets/desktop/desktopLight.png)`,
-                dark: 'url(./assets/desktop/desktopDark.png)'
-            }
+                dark: 'url(./assets/desktop/desktopDark.png)',
+            },
         },
     },
     plugins: [],
